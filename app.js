63386964304e1dbc60315368133c9901dda7a938
lakshay-pant <pantlakshay2@gmail.com>
2021-12-01 10:39:00 +0530

    server timeout

--- app.js
+++ app.js
@@ -10,11 +10,6 @@ const multer = require('multer');
 var upload = multer({ dest: 'uploads/' });
 
 //API security
-app.use(function (req, res) {
-	var delayed = new DelayedResponse(req, res);
-	// verySlowFunction can now run indefinitely
-	verySlowFunction(delayed.start());
-});
 
 // app.use(express.static(__dirname+"uploads/"));
 const port = process.env.PORT || 3001;
