bb4a189638944d68891926890ba963a2103c328d
lakshay-pant <pantlakshay2@gmail.com>
2021-12-05 15:00:14 +0530

    ss

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ if (process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+		//res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+		res.sendFile(path.join(__dirname + '/client/build/index.html'));
 	});
 }
 
