266dbdb6d81c5c0ddbdb90d5f5e7bdaceffdd710
lakshay-pant <pantlakshay2@gmail.com>
2022-02-01 09:51:07 +0530

    ss

--- client/src/pages/allStudents/allStudentAction.js
+++ client/src/pages/allStudents/allStudentAction.js
@@ -1,26 +1,23 @@
 import {
-    fetchStudentLoading,
-    fetchStudentSuccess,
-    fetchStudentFail,
-    searchStudents
-    
-  } from "./allStudentSlice";
-  
-  import {
-    getAllUserStudents
-  } from "../../api/studentApi";
-  
-  export const fetchAllStudents = () => async (dispatch) => {
-    dispatch(fetchStudentLoading());
-    try {
-      const result = await getAllUserStudents();
-      result.data.result.length &&
-        dispatch(fetchStudentSuccess(result.data.result));
-    } catch (error) {
-      dispatch(fetchStudentFail(error.message));
-    }
-  };
+	fetchStudentLoading,
+	fetchStudentSuccess,
+	fetchStudentFail,
+	searchStudents,
+} from './allStudentSlice';
 
-  export const filterSerachStudent = (str) => (dispatch) => {
-    dispatch(searchStudents(str));
-  };
+import { getAllStudents } from '../../api/studentApi';
+
+export const fetchAllStudents = () => async (dispatch) => {
+	dispatch(fetchStudentLoading());
+	try {
+		const result = await getAllStudents();
+		result.data.result.length &&
+			dispatch(fetchStudentSuccess(result.data.result));
+	} catch (error) {
+		dispatch(fetchStudentFail(error.message));
+	}
+};
+
+export const filterSerachStudent = (str) => (dispatch) => {
+	dispatch(searchStudents(str));
+};
